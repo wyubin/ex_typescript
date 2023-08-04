f5f9ee82e006ef4cee514d94e2a413c9106e66fc
shaohua1202 <shaohuawang1202@gmail.com>
2023-08-04 11:07:24 +0000

    for_loop practice

---
+++ for_loop.ts
@@ -0,0 +1,14 @@
+//輸出: 0~19
+for(var a=0 ; a<20 ; a++){
+    console.log(a)
+}
+
+//使用者為shaohua,輸出登入成功
+//如非為shaohua,輸出登入失敗
+var name: string = "shaohua"
+if (name ==="shaohua"){
+    console.log("登入成功")
+    
+}else{
+    console.log("登入失敗")
+}
