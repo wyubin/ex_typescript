3e589cd3d029a1f4a80d062bbe8784fd8818165d
shaohua1202 <shaohuawang1202@gmail.com>
2023-08-06 06:36:36 +0000

    update

---
+++ practiced/object.ts
@@ -0,0 +1,7 @@
+let boy:object = {
+    firstname: 'shaohua',
+    lastname: 'wang',
+    age: 14,
+    marriage: undefined,
+    gender: null,
+  };
---
+++ practiced/variables.ts
@@ -0,0 +1,19 @@
+// 數字變數
+let a:number = 6
+// 字串變數
+let b:string = "shaohua"
+// 句子與字串組合變數
+let Hello:string = `Hi my name is ${b}`
+// 布林變數
+let ate_rice:boolean = true
+// 輸出L6的句子
+console.log(Hello)
+//設空值 並輸出
+let c
+console.log(c)
+
+let u: undefined = undefined;
+let n: null = null;
+
+let num: number;
+let str: string;
