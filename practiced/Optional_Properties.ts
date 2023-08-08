ed332388549cce0531da623a7139dcab1c0b2e03
shaohua <shaohuawang1202@gmail.com>
2023-08-08 18:13:48 +0800

    update 8/8

---
+++ practiced/Optional_Properties.ts
@@ -0,0 +1,32 @@
+//選擇性屬性 變數加上?
+interface number_test{
+    x:number;
+    y?:number;
+}
+//類別
+let employee= {
+    empId : 1 ,
+    empName : "shaohua"
+    showInfo : function(){}
+}
+
+employee.showInfo = ()=>{
+    console.log(employee.empId+"-"+employee.empName);
+}
+
+class Employee {
+
+    public a: number;
+    protected b: number;
+    private c: number;
+
+    constructor(first: number, second: number, third: number) {
+        this.a = first;
+        this.b = second;
+        this.c = third;
+}
+
+    showInfo() {
+        return this. a + "-" + this.b + "-" + this.c ;
+    };
+}
